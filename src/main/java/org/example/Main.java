5f1a17ad6bb5a41abff327aa46382d4c02325b34
kofoworola <kay16th@yahoo.com>
2024-11-22 11:17:13 +0000

    feat: Retrieves a SessionFactory object from Database class
    Calls the appropriate method to export the schema

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Retrieves a SessionFactory object from your Database class
+        SessionFactory session = Database.getSessionFactory();
+
+        //export the schema, i.e. to automatically create any necessary tables in your in-memory database.
+        session.getSchemaManager().exportMappedObjects(true);
     }
 }
